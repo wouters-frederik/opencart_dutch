0ec953e1943bc0ed2d0ef950a261a6b7c3b5a6c1
Frederik Wouters <wouters.f@gmail.com>
2013-01-14 21:51:44 +0100

    added more translations

--- admin/language/dutch/payment/europabank.php
+++ admin/language/dutch/payment/europabank.php
@@ -8,15 +8,17 @@ $_['text_success']       = 'Success: You have modified Europabank account detail
 $_['text_europabank']     = '<a onclick="window.open(\'https://www.europabank.be/\');"><img src="view/image/payment/europabank.jpg" alt="Europabank" title="TargetPay" style="border: 1px solid #EEEEEE;" /></a>';
 
 // Entry
-$_['entry_layoutcode']   = 'Layout-code:';
-$_['entry_testmode']     = 'Testmode:';
-$_['entry_order_status'] = 'Order Status:';
-$_['entry_geo_zone']     = 'Geo Zone:';
-$_['entry_status']       = 'Status:';
-$_['entry_sort_order']   = 'Sort Order:';
-$_['entry_author']       = 'Author:';
+$_['entry_mpi']   = 'MPI-account number';
+$_['entry_cur']   = 'Currency code';
+$_['entry_url']   = 'Payment provider url (URL mpi), defaults to https://www.ebonline.be/mpi/authenticate';
+$_['entry_cs']    = 'Client secret';
+$_['entry_ss']    = 'Server secret';
+$_['entry_proxy']  = 'Proxy url';
+$_['entry_lan']   = 'Default language';
+$_['entry_env']   = 'Europabank environment';
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify payment Europabank!';
-$_['error_layoutcode']   = 'Layout-code Required!';
+$_['error_entry_mpi']   = 'MPI number Required!';
+$_['error_field_required']   = 'Required field!';
 ?>
